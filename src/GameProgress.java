fc9f286739715c548e24b2c7eaf88254c9bd2f1b
Soul <top.secret.work@yandex.ru>
2021-12-02 17:19:40 +0300

    Update

--- src/GameProgress.java
+++ src/GameProgress.java
@@ -1,4 +1,9 @@
 import java.io.*;
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 public class GameProgress implements Serializable {
     private static final long serialVersionUID = 1L;
@@ -30,7 +35,100 @@ public class GameProgress implements Serializable {
         }
     }
 
-    public void
+    public void zipFiles(String zipPath, String... listObj) {
+
+        HashMap<String, String> listObjNames = new HashMap<>();
+
+        for (String name : listObj) {
+            String[] splitName = name.split(Pattern.quote("\\"));
+            Collections.reverse(Arrays.asList(splitName));
+            listObjNames.put(splitName[0], name);
+        }
+
+        // запись в архив
+        try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipPath))) {
+
+            for (Map.Entry<String, String> entry : listObjNames.entrySet()) {
+                File file = new File(entry.getValue());
+                FileInputStream fis = new FileInputStream(file);
+
+                ZipEntry ze = new ZipEntry(entry.getKey());
+                zos.putNextEntry(ze);
+
+                if (file.exists() && file.isFile()) {
+                    byte[] buffer = new byte[fis.available()];
+                    fis.read(buffer);
+                    zos.write(buffer);
+                    logger.addLog("файл (" + entry.getValue() + ") заархивирован в " + zipPath, true);
+                } else {
+                    logger.addLog("ошибка при архивации, файл (" + entry.getValue() + ") не существует или не является файлом", false);
+                }
+                zos.closeEntry();
+                fis.close();
+            }
+
+        } catch (IOException e) {
+            logger.addLog("ошибка при архивации - " + e.getMessage(), false);
+        }
+
+        // удаление архивированных файлов
+        try (ZipInputStream zis = new ZipInputStream(new FileInputStream(zipPath))) {
+
+            ZipEntry entry;
+            String fileName;
+
+            while ((entry = zis.getNextEntry()) != null) {
+                fileName = entry.getName();
+                if (listObjNames.containsKey(fileName)) {
+                    File delFile = new File(listObjNames.get(fileName));
+                    if (delFile.delete()) {
+                        logger.addLog("файл (" + listObjNames.get(fileName) + ") удален", true);
+                    } else {
+                        logger.addLog("файл (" + listObjNames.get(fileName) + ") не удален", false);
+                    }
+                }
+
+                zis.closeEntry();
+            }
+        } catch (IOException e) {
+            logger.addLog("удаление файлов вне архива", false);
+        }
+    }
+
+    public void openZip(String zipPath, String path) {
+        try (ZipInputStream zis = new ZipInputStream(new FileInputStream(zipPath))) {
+            ZipEntry entry;
+            String fileName;
+            while ((entry = zis.getNextEntry()) != null) {
+                fileName = entry.getName();
+                FileOutputStream fos = new FileOutputStream(path + "\\" + fileName);
+                for (int c = zis.read(); c != -1; c = zis.read()) {
+                    fos.write(c);
+                }
+                fos.flush();
+                zis.closeEntry();
+                fos.close();
+                logger.addLog("файл (" + fileName + ") разархивирован в " + path, true);
+            }
+        } catch (IOException e) {
+            logger.addLog("архив (" + zipPath + ") не удалось прочитать", false);
+        }
+    }
+
+    public GameProgress openProgress(String objPath) {
+        GameProgress obj = null;
+        try (
+                FileInputStream fis = new FileInputStream(objPath);
+                ObjectInputStream ois = new ObjectInputStream(fis)
+            ) {
+            obj = (GameProgress) ois.readObject();
+            logger.addLog("файл (" + objPath + ") дессерилизован", true);
+        } catch (IOException | ClassNotFoundException e) {
+            logger.addLog("не удалось дессерилиазовать (" + objPath + ")", false);
+        }
+
+        return obj;
+    }
 
     @Override
     public String toString() {
--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,31 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         String mainPath = System.getProperty("user.dir") + "\\Games";
+        String saveGamePath = mainPath + "\\savegames";
+        String zipPath = mainPath + "\\savegames\\zip.zip";
+        String savePathPlayer = mainPath + "\\savegames\\player.dat";
+        String savePathPlayer2 = mainPath + "\\savegames\\player2.dat";
+        String savePathPlayer3 = mainPath + "\\savegames\\player3.dat";
+
         InstallGame installGame = new InstallGame(mainPath);
 
         GameProgress player = new GameProgress(100, 3, 23, 163.45);
         GameProgress player2 = new GameProgress(93, 2, 11, 1543.98);
         GameProgress player3 = new GameProgress(65, 6, 54, 122.75);
 
-        String savePathPlayer = mainPath + "\\savegames\\player.dat";
-        String savePathPlayer2 = mainPath + "\\savegames\\player2.dat";
-        String savePathPlayer3 = mainPath + "\\savegames\\player3.dat";
-
         player.saveGame(savePathPlayer);
         player2.saveGame(savePathPlayer2);
         player3.saveGame(savePathPlayer3);
+
+        player.zipFiles(zipPath, savePathPlayer, savePathPlayer2, savePathPlayer3);
+        player.openZip(zipPath, saveGamePath);
+
+        GameProgress save = player.openProgress(savePathPlayer3);
+        GameProgress save2 = player.openProgress(savePathPlayer2);
+        GameProgress save3 = player.openProgress(savePathPlayer);
+
+        System.out.println(save);
+        System.out.println(save2);
+        System.out.println(save3);
     }
 }
